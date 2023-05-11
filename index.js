da7d0cbc1b7e532b5f9ee7df4624130905551e5c
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-11 20:32:56 -0300

    config and fetch api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+import express from 'express';
+import cors from 'cors';
+import {projectId, privateKey} from './secret/ids.js';
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
@@ -7,7 +9,18 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+      const r = axios.put(
+          'https://api.chatengine.io/users/',
+          {username: username, secret: username, first_name: username},
+          {headers: {"private-key": privateKey }}
+      )
+      return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
 });
 
 app.listen(3001);
+
